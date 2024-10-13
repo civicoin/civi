feaf3f2eae5a8bd184dfd795387612b5d6aebfd6
Ilya Zaytsev <ilya@zaytsev.xyz>
2024-10-14 01:21:36 +0300

    export hmac

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { hmac, getMessageWithHMAC, verifyMessageWithHMAC } from './utils/auth/hmac'
+
+export { hmac, getMessageWithHMAC, verifyMessageWithHMAC }
--- utils/auth/hmac.ts
+++ utils/auth/hmac.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto'
 
-const hmac = (message: object, key: string) =>
+export const hmac = (message: object, key: string) =>
 	crypto.createHmac('sha256', key).update(JSON.stringify(message)).digest('hex')
 
 export const getMessageWithHMAC = (message: object, key: string) => ({
@@ -13,5 +13,3 @@ export const verifyMessageWithHMAC = (message: { hmac?: string }, key: string) =
 	const { hmac: messageHMAC, ...rest } = message
 	return messageHMAC === hmac(rest, key)
 }
-
-export default hmac
